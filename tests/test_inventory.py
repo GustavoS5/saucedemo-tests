f1dbc4a81195743fd1d20b2cf4b93b8b0ee99549
Berilium <jgusta.s25@gmail.com>
2026-06-20 19:54:40 -0300

    Refactor add item to cart test to use inventory_page fixture

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -5,10 +5,11 @@ from __future__ import annotations
 import pytest
 from playwright.sync_api import expect
 from pages.inventory_page import InventoryPage
+from tests.conftest import inventory_page
+
 
 @pytest.mark.parametrize("item_name", ["Sauce Labs Backpack", "Sauce Labs Bike Light"])
-def test_add_item_to_cart(logged_in_page, item_name):
+def test_add_item_to_cart(inventory_page, item_name):
     """Test that an item can be added to the cart."""
-    inventory_page = InventoryPage(logged_in_page)
     inventory_page.add_item_to_cart(item_name)
     expect(inventory_page.shopping_cart_badge).to_have_text("1")
