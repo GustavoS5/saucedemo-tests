90b479a8a1f3d7111ad8d2f413e39e71c63be6f2
Berilium <jgusta.s25@gmail.com>
2026-06-20 19:13:03 -0300

    Add test for valid login credentials in end-to-end tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,5 +2,15 @@
 
 from __future__ import annotations
 
+import pytest
 from playwright.sync_api import expect
+from pages.login_page import LoginPage
 
+
+def test_login_valid_credentials(saucedemo_credentials, login_page: LoginPage):
+    """Test that a user can log in with valid credentials."""
+    login_page.load()
+    login_page.login(
+        saucedemo_credentials["username"], saucedemo_credentials["password"]
+    )
+    expect(login_page.page).to_have_url("/inventory.html")
