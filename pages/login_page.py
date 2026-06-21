affd2ccd99be51f098ddb62110cef78a4cfdd0ec
Berilium <jgusta.s25@gmail.com>
2026-06-20 23:55:44 -0300

    Update error message locator in login page for consistency

--- pages/login_page.py
+++ pages/login_page.py
@@ -15,7 +15,7 @@ class LoginPage(BasePage):
         self.username_input = page.get_by_placeholder("Username")
         self.password_input = page.get_by_placeholder("Password")
         self.login_button = page.get_by_role("button", name="Login")
-        self.error_message = page.get_by_test_id("error-message")
+        self.error_message = page.get_by_test_id("error")
 
     def load(self):
         """Navigate to the login page."""
