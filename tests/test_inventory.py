40185f0d1b9752fc24fa44468cd22c4a8b416fee
Berilium <jgusta.s25@gmail.com>
2026-06-20 23:55:29 -0300

    Refactor inventory tests to improve clarity and remove unnecessary comments

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -4,24 +4,22 @@ from __future__ import annotations
 
 import pytest
 from playwright.sync_api import expect
-from pages.inventory_page import InventoryPage
 from tests.conftest import inventory_page
 
 
 @pytest.mark.parametrize("item_name", ["Sauce Labs Backpack", "Sauce Labs Bike Light"])
 def test_add_item_to_cart(inventory_page, item_name):
-    """Test that an item can be added to the cart."""
     inventory_page.add_item_to_cart(item_name)
     expect(inventory_page.shopping_cart_badge).to_have_text("1")
 
+
 def test_add_multiple_items_to_cart(inventory_page):
-    """Test that multiple items can be added to the cart."""
     inventory_page.add_item_to_cart("Sauce Labs Backpack")
     inventory_page.add_item_to_cart("Sauce Labs Bike Light")
     expect(inventory_page.shopping_cart_badge).to_have_text("2")
 
+
 def test_remove_item_from_cart(inventory_page):
-    """Test that an item can be removed from the cart."""
     inventory_page.add_item_to_cart("Sauce Labs Backpack")
     inventory_page.remove_item_from_cart("Sauce Labs Backpack")
     expect(inventory_page.shopping_cart_badge).to_be_hidden()
